b358d40a3a176840f524b93bc66062e4cca37d03
Simon Louail <simon.louail@econocom.com>
2021-08-12 16:44:51 +0200

    init repository with Spring boot project @ <type>(<scope>): <subject> @ @ <body> @ @ <footer> @ @ Type : feat, fix, docs, style, refactor, perf, test, build, ci, chore @ Body : Should include the motivation for the change and contrast this with previous behavior @ Foot : Issues that this commit closes and the BREAKING CHANGES

---
+++ src/test/java/com/test/budgetbox/BudgetboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.budgetbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
